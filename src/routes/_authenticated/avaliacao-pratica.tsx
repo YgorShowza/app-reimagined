21ee887099025e7871eee5cb6538609c411390c3
YgorShowza <ygoxxx@gmail.com>
2026-09-09 21:06:07 -0300

    Polish practical assessment page hierarchy

--- src/routes/_authenticated/avaliacao-pratica.tsx
+++ src/routes/_authenticated/avaliacao-pratica.tsx
@@ -1,17 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { PracticalRecurrencePanel } from "@/components/practical/PracticalRecurrencePanel";
 import { PracticalWorkspace } from "@/components/practical/PracticalWorkspace";
-
-function PracticalAdminPage() {
-  return (
-    <div className="segempat-training-practical">
-      <PracticalRecurrencePanel />
-      <PracticalWorkspace />
-    </div>
-  );
-}
 
 export const Route = createFileRoute("/_authenticated/avaliacao-pratica")({
   head: () => ({ meta: [{ title: "Avaliação Prática · SEGEMPAT" }] }),
-  component: PracticalAdminPage,
+  component: PracticalWorkspace,
 });
